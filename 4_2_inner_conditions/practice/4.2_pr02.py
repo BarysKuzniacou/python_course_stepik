1eaefcb4e3954b0ca7a96ae148fe2197425f9b83
Barys Kuzniacou <barys.kuzniacou@gmail.com>
2025-06-04 22:28:37 +0300

    4.2 practice 5

---
+++ 4_2_inner_conditions/practice/4.2_pr02.py
@@ -0,0 +1,35 @@
+# На вход программе подаются три целых числа, записанных в одну строку через пробел. Необходимо прочитать их и определить наименьшее среди прочитанных чисел. Наименьшее найденное значение вывести на экран.
+#
+# P.S. Программу реализовать следует, используя условный оператор, без использования функции min.
+#
+# Тесты: https://github.com/selfedu-rus/test-python-base/tree/main/4/4.2.2
+#
+# Sample Input:
+# 8 11 -1
+# Sample Output:
+# -1
+#
+# test #2
+# input: 7 3 2
+# output: 2
+#
+# test #3
+# input: -1 -11 -5
+# output: -11
+#
+# test #4
+# input: 2 2 5
+# output: 2
+#
+# test #5
+# input: 3 3 3
+# output: 3
+
+a, b, c = map(int, input().split())
+
+if a <= b and a <= c:
+    print(a)
+elif b <= a and b <= c:
+    print(b)
+else:
+    print(c)
---
+++ 4_2_inner_conditions/practice/4.2_pr06.py
@@ -0,0 +1,51 @@
+# На вход программе подаются два целых числа, записанных через пробел. Необходимо прочитать эти числа по порядку в
+# переменные m (порядковый номер месяца) и n (число, день месяца). Затем, по переменным m и n определить:
+#
+# а) дату предыдущего дня (принять, что m и n не могут являться 1 января);
+# б) дату следующего дня (принять, что m и n не могут являться 31 декабря).
+#
+# В задаче принять, что год не является високосным. Вывести предыдущую дату и следующую дату (в формате: mm.dd,
+# где m - число месяца; d - номер дня) в одну строчку через пробел.
+#
+# P.S. Число дней в месяцах не високосного года, начиная с января: 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+#
+# Тесты: https://github.com/selfedu-rus/test-python-base/tree/main/4/4.2.6
+#
+# Sample Input:
+# 8 31
+# Sample Output:
+# 08.30 09.01
+
+m, n = map(int, input().split())
+
+if m == 1 or m == 3 or m == 5 or m == 7 or m == 8 or m == 10 or m == 12:
+    if 1 < n < 31:
+        day_before = n - 1
+        day_after = n + 1
+        print(f'{m:02}.{day_before:02}' + ' ' + f'{m:02}.{day_after:02}')
+    elif n == 31:
+        day_before = 30
+        day_after = 1
+        print(f'{m:02}.{day_before:02}' + ' ' + f'{(m + 1):02}.{day_after:02}')
+    else:
+
+elif m == 2:
+    if 1 < n < 28:
+        day_before = n - 1
+        day_after = n + 1
+        print(f'{m:02}.{day_before:02}' + ' ' + f'{m:02}.{day_after:02}')
+    else:
+        day_before = 27
+        day_after = 1
+        print(f'{m:02}.{day_before:02}' + ' ' + f'{(m + 1):02}.{day_after:02}')
+else:
+    if n < 30:
+        day_before = n - 1
+        day_after = n + 1
+        print(f'{m:02}.{day_before:02}' + ' ' + f'{m:02}.{day_after:02}')
+    else:
+        day_before = 29
+        day_after = 1
+        print(f'{m:02}.{day_before:02}' + ' ' + f'{(m + 1):02}.{day_after:02}')
+
+
