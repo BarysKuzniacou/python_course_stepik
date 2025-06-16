364788f744f707ad1e60d13b16eb48e237749bf8
Barys Kuzniacou <barys.kuzniacou@gmail.com>
2025-06-16 21:31:41 +0300

    5.1 practice 3

---
+++ 5_1_while/5.1_ex01.py
@@ -0,0 +1,15 @@
+sum = 0
+i = 0
+n = 1000
+
+while i <= n:
+    sum += i
+    print(i, sum)
+    i += 1
+
+i = -1
+n = -10
+
+while i > n:
+    print(i)
+    i -= 1
---
+++ 5_1_while/5.1_ex02.py
@@ -0,0 +1,7 @@
+pass_true = 'password'
+ps = ''
+
+while pass_true != ps:
+    ps = input('password: ')
+
+print('password true')
