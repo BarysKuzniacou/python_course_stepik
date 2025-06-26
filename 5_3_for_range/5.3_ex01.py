a3fb31b8970cba4527af1f964518833b6b14816e
Barys Kuzniacou <barys.kuzniacou@gmail.com>
2025-06-26 23:27:46 +0300

    5.3 ex

---
+++ 5_3_for_range/5.3_ex01.py
@@ -0,0 +1,12 @@
+lst = [1, 2, 3, 4, 5]
+
+for i in lst:
+    print(i)
+
+for i in 'ABCDEFG':
+    print(i)
+
+for i in lst:
+    i = 0
+
+print(*lst)
---
+++ 5_3_for_range/5.3_ex02_range.py
@@ -0,0 +1,9 @@
+# range (start, stop, step)
+# range (start, stop)
+# range (start)
+
+#lst = [1, 2, 3, 4, 5]
+
+print(range(5))
+
+# 8-20
