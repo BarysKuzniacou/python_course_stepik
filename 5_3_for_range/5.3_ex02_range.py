426625753f04e5c69290c25f1227091c1392047b
Barys Kuzniacou <barys.kuzniacou@gmail.com>
2025-07-01 21:31:15 +0300

    5.3 practice 6

--- 5_3_for_range/5.3_ex02_range.py
+++ 5_3_for_range/5.3_ex02_range.py
@@ -2,8 +2,18 @@
 # range (start, stop)
 # range (start)
 
-#lst = [1, 2, 3, 4, 5]
+lst = [1, 2, 3, 4, 5]
 
 print(range(5))
+print(list(range(5)))
+print(list(range(0)))
+print(list(range(0, -5)))
+print(list(range(-10, -5)))
+print(list(range(-10, -5, 2)))
+print(list(range(-10, -5, -2)))
+print(list(range(-5, -10, -2)))
 
-# 8-20
+
+
+for i in range(len(lst)):
+    print(lst[i])
---
+++ 5_3_for_range/5.3_ex03.py
@@ -0,0 +1,6 @@
+sum = 0
+
+for i in range(2, 1001):
+    sum += 1/i
+
+print(sum)
