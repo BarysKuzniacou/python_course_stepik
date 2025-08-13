577fc2e84786d5fda7713795f83ea9c58c0b7d36
Barys Kuzniacou <barys.kuzniacou@gmail.com>
2025-08-13 22:38:32 +0300

    5.6 pr04 tests

--- 5_6_nested_loops/practice/p5_6_pr04.py
+++ 5_6_nested_loops/practice/p5_6_pr04.py
@@ -15,6 +15,14 @@
 # Sample Output:
 # ДА
 
-def units_do_not_touch(lst_in):
+def units_do_not_touch(lst_in, n):
+    for i in range(0, len(lst_in)-1):
+        for j in range(1, len(lst_in[i])):
+            sum = lst_in[i][j-1] + lst_in[i][j] + lst_in[i+1][j-1] + lst_in[i+1][j]
+            if sum > 1:
+                print('test ' + str(n) + ' НЕТ')
+                return 'НЕТ'
+
+    print('test ' + str(n)+ ' ДА')
 
     return 'ДА'
--- 5_6_nested_loops/practice/p5_6_pr04_test.py
+++ 5_6_nested_loops/practice/p5_6_pr04_test.py
@@ -3,69 +3,90 @@ from p5_6_pr04 import units_do_not_touch
 
 class TestUnitsTouch(unittest.TestCase):
 
-    def test_units_touch(self):
-        self.assertEqual(units_do_not_touch([[1, 0, 0, 0, 0], # ДА
-                                             [0, 0, 1, 0, 1],
-                                             [0, 0, 0, 0, 0],
-                                             [0, 1, 0, 1, 0],
-                                             [0, 0, 0, 0, 0]]), 'ДА')
+    def test_units_touch_01(self):
+        self.assertEqual(units_do_not_touch([
+                                            [1, 0, 0, 0, 0],
+                                            [0, 0, 1, 0, 1],
+                                            [0, 0, 0, 0, 0],
+                                            [0, 1, 0, 1, 0],
+                                            [0, 0, 0, 0, 0]], 1), 'ДА')
 
-        self.assertEqual(units_do_not_touch([[1, 0, 0, 0, 0,],
-                                             [0, 1, 0, 0, 1],
-                                             [0, 0, 0, 0, 0],
-                                             [0, 1, 0, 0, 0],
-                                             [0, 0, 0, 1, 0]]), 'НЕТ')
+    def test_units_touch_02(self):
+        self.assertEqual(units_do_not_touch([
+                                            [1, 0, 0, 0, 0],
+                                            [0, 1, 0, 0, 1],
+                                            [0, 0, 0, 0, 0],
+                                            [0, 1, 0, 0, 0],
+                                            [0, 0, 0, 1, 0]], 2), 'НЕТ')
 
-        self.assertEqual(units_do_not_touch([[1, 0, 0, 0, 0],
-                                             [0, 0, 0, 0, 1],
-                                             [0, 0, 1, 0, 0],
-                                             [0, 0, 0, 0, 0],
-                                             [1, 0, 0, 0, 1]]), 'ДА')
+    def test_units_touch_03(self):
+        self.assertEqual(units_do_not_touch([
+                                            [1, 0, 0, 0, 0],
+                                            [0, 0, 0, 0, 1],
+                                            [0, 0, 1, 0, 0],
+                                            [0, 0, 0, 0, 0],
+                                            [1, 0, 0, 0, 1]], 3), 'ДА')
 
-        self.assertEqual(units_do_not_touch([[0, 0, 0, 0, 0],
-                                             [0, 0, 1, 0, 1],
-                                             [0, 0, 1, 0, 0],
-                                             [0, 1, 0, 1, 0],
-                                             [0, 0, 0, 0, 0]]), 'НЕТ')
+    def test_units_touch_04(self):
+        self.assertEqual(units_do_not_touch([
+                                            [0, 0, 0, 0, 0],
+                                            [0, 0, 1, 0, 1],
+                                            [0, 0, 1, 0, 0],
+                                            [0, 1, 0, 1, 0],
+                                            [0, 0, 0, 0, 0]], 4), 'НЕТ')
 
-        self.assertEqual(units_do_not_touch([[1, 1, 1, 0, 0],
-                                             [0, 0, 0, 0, 1],
-                                             [0, 0, 0, 0, 0],
-                                             [0, 1, 0, 1, 0],
-                                             [0, 0, 0, 0, 0]]), 'НЕТ')
+    def test_units_touch_05(self):
+        self.assertEqual(units_do_not_touch([
+                                            [1, 1, 1, 0, 0],
+                                            [0, 0, 0, 0, 1],
+                                            [0, 0, 0, 0, 0],
+                                            [0, 1, 0, 1, 0],
+                                            [0, 0, 0, 0, 0]], 5), 'НЕТ')
 
-        self.assertEqual(units_do_not_touch([[1 0 0 0 0
-1 0 1 0 1
-1 0 0 0 0
-1 1 0 1 0
-1 0 0 0 0]]), 'НЕТ')
+    def test_units_touch_06(self):
+        self.assertEqual(units_do_not_touch([
+                                            [1, 0, 0, 0, 0],
+                                            [1, 0, 1, 0, 1],
+                                            [1, 0, 0, 0, 0],
+                                            [1, 1, 0, 1, 0],
+                                            [1, 0, 0, 0, 0]], 6), 'НЕТ')
 
-        self.assertEqual(units_do_not_touch([[1 0 0 1 1
-0 0 0 0 1
-0 0 0 0 0
-0 1 0 1 0
-0 0 0 0 0]]), 'НЕТ')
+    def test_units_touch_07(self):
+        self.assertEqual(units_do_not_touch([
+                                            [1, 0, 0, 1, 1],
+                                            [0, 0, 0, 0, 1],
+                                            [0, 0, 0, 0, 0],
+                                            [0, 1, 0, 1, 0],
+                                            [0, 0, 0, 0, 0]], 7), 'НЕТ')
 
-        self.assertEqual(units_do_not_touch([[1 0 0 0 1
-0 0 1 0 1
-0 0 0 0 1
-0 1 0 0 1
-0 0 0 0 1]]), 'НЕТ')
+    def test_units_touch_08(self):
+        self.assertEqual(units_do_not_touch([
+                                            [1, 0, 0, 0, 1],
+                                            [0, 0, 1, 0, 1],
+                                            [0, 0, 0, 0, 1],
+                                            [0, 1, 0, 0, 1],
+                                            [0, 0, 0, 0, 1]], 8), 'НЕТ')
 
-        self.assertEqual(units_do_not_touch([[1 0 0 0 0
-0 0 1 0 1
-0 0 0 0 0
-0 1 0 0 0
-1 1 1 1 1]]), 'НЕТ')
+    def test_units_touch_09(self):
+        self.assertEqual(units_do_not_touch([
+                                            [1, 0, 0, 0, 0],
+                                            [0, 0, 1, 0, 1],
+                                            [0, 0, 0, 0, 0],
+                                            [0, 1, 0, 0, 0],
+                                            [1, 1, 1, 1, 1]], 9), 'НЕТ')
 
-        self.assertEqual(units_do_not_touch([[0 0 0 0 1
-0 0 0 1 0
-0 0 1 0 0
-0 1 0 0 0
-1 0 0 0 0]]), 'НЕТ')
+    def test_units_touch_10(self):
+        self.assertEqual(units_do_not_touch([
+                                            [0, 0, 0, 0, 1],
+                                            [0, 0, 0, 1, 0],
+                                            [0, 0, 1, 0, 0],
+                                            [0, 1, 0, 0, 0],
+                                            [1, 0, 0, 0, 0]], 10), 'НЕТ')
 
-        self.assertEqual(units_do_not_touch([[0 0 1 0 1
-0 1 0 0 0
-1 0 0 0 1
-0 0 0 1 0
-1 0 1 0 0]]), 'НЕТ')
+    def test_units_touch_11(self):
+        self.assertEqual(units_do_not_touch([
+                                            [0, 0, 1, 0, 1],
+                                            [0, 1, 0, 0, 0],
+                                            [1, 0, 0, 0, 1],
+                                            [0, 0, 0, 1, 0],
+                                            [1, 0, 1, 0, 0]], 11), 'НЕТ')
