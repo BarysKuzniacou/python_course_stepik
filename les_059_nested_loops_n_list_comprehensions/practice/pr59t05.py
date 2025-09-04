48bd953600900fe33da96c627cad9ddd79cf0aa6
Barys Kuzniacou <barys.kuzniacou@gmail.com>
2025-09-04 21:22:29 +0300

    5.9 pr 05

--- les_059_nested_loops_n_list_comprehensions/practice/pr59t05.py
+++ les_059_nested_loops_n_list_comprehensions/practice/pr59t05.py
@@ -23,3 +23,13 @@
 # 1 4 7 5
 # 2 5 8 4
 # 3 6 9 3
+
+lst_in = [[1, 2, 3],
+          [4, 5, 6],
+          [7, 8, 9],
+          [5, 4, 3]]
+
+A = [[lst_in[i][j] for i in range(len(lst_in))] for j in range(len(lst_in[0]))]
+
+for row in A:
+    print(*row)
