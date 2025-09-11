78821a0c50695d4c03cd81c1e95772f5f1131e5e
Barys Kuzniacou <barys.kuzniacou@gmail.com>
2025-09-11 21:33:43 +0300

    pr 6.1 t 02

--- les_61_dict/practice/pr61t02.py
+++ les_61_dict/practice/pr61t02.py
@@ -21,3 +21,39 @@
 # 3=удовлетворительно
 # Sample Output:
 # (3, 'удовлетворительно') (4, 'хорошо') (5, 'отлично')
+
+lst_in = ['5=отлично', '4=хорошо', '3=удовлетворительно']
+
+lst = []
+
+d ={}
+
+for i in range(len(lst_in)):
+    lst.append((str(lst_in[i]).replace("=", ' ')).split())
+
+for i in range(0, len(lst)):
+    d[int(lst[i][0])] = lst[i][1]
+
+print(*sorted(d.items()))
+
+# d = {}
+# for i in lst_in:
+#     key, value = i.split('=')
+#     d[int(key)] = value
+#
+# print(*sorted(d.items()))
+
+# d = {}
+# for s in lst_in:
+#     row = s.split('=')
+#     d[int(row[0])] = row[1]
+#
+# print(*sorted(d.items()))
+
+# d = {}
+# for i in lst_in:
+#   d[int(i[0])] = i[2:]
+#
+# print(*sorted(d.items()))
+
+
