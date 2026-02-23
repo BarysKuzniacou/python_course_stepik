2acf227edd484bcdde9cba85703fce341c1aaf45
Barys Kuzniacou <barys.kuzniacou@gmail.com>
2026-02-23 20:51:13 +0300

    practice 7.4: task 4, 5

--- les_74_positional_and_named_arguments/practice/pr74t03_test.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from les_74_positional_and_named_arguments.practice.pr74t03 import get_latin
-
-class TestGetLatin(unittest.TestCase):
-
-    def test_get_latin(self):
-        self.assertEqual(get_latin('Лучший курс по Python!'), 'luchshiy-kurs-po-python!')
-        self.assertEqual(get_latin('Лучший курс по Python!', '+'), 'luchshiy+kurs+po+python!')
