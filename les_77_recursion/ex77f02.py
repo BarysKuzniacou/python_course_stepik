66492ff94c6884aceda41ec58317a3d9ecb41c1a
Barys Kuzniacou <barys.kuzniacou@gmail.com>
2026-03-09 21:08:35 +0300

    ex 7.7 recursion practice 7.7: task 1

---
+++ les_77_recursion/ex77f02.py
@@ -0,0 +1,28 @@
+def set_zeros(p, i, j, sz):
+    if p[i][j] != '*':
+        return
+    
+    p[i][j] = '@'
+    
+    if i-1 >= 0 and p[i-1][j] == '*':
+        set_zeros(p, i-1, j, sz)
+    if i+1 < sz and p[i+1][j] == '*':
+        set_zeros(p, i+1, j, sz)
+    if j-1 >= 0 and p[i][j-1] == '*':
+        set_zeros(p, i, j-1, sz)
+    if j+1 < sz and p[i][j+1] == '*':
+        set_zeros(p, i, j+1, sz)
+
+
+pole = [
+    ['#', '*', '*', '*', '#'],
+    ['#', '#', '#', '#', '#'],
+    ['*', '*', '*', '*', '#'],
+    ['#', '#', '*', '*', '*'],
+    ['#', '#', '*', '#', '#'],
+]
+
+N = len(pole)
+set_zeros(pole, 2, 2, N)
+
+print(*pole, sep='\n')
