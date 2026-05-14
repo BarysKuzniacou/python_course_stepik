d1bf6855bc35e2c8c35154f99ac00f59307654a5
Barys Kuzniacou <barys.kuzniacou@gmail.com>
2026-05-14 21:02:49 +0300

    7.10 lambda practice: question 1; task 1, 2

---
+++ les_710_lambda/practice/pr710q01.py
@@ -0,0 +1,17 @@
+# [ok] lambda x: x
+
+# [ok] lambda x, y: x+y
+
+# [ok] lambda a: -a
+
+# [ok] lambda: "hello lambda"
+
+# lambda x: return x
+
+# lambda: return "hello lambda"
+
+# lambda a, b: a, b = b, a
+
+# lambda d: d += 1
+
+# lambda d: (d += 1)
