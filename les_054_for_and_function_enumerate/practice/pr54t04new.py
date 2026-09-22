207cf586df673755f2cc1cf3f622554b46390a72
Barys Kuzniacou <barys.kuzniacou@gmail.com>
2026-09-22 21:54:25 +0300

    practice 5.4, new tasks 4, ex file 6

--- les_054_for_and_function_enumerate/practice/pr54t04new.py
+++ les_054_for_and_function_enumerate/practice/pr54t04new.py
@@ -30,7 +30,7 @@ matrix = [
     [13, 12, 11, 10, 9]
 ]
 
-# N = 5
+N = 5
 
 # matrix = [[0] * N for n in range(N)]
 
@@ -46,10 +46,13 @@ matrix = [
 # print(matrix)
 
 print(matrix[0][0], matrix[0][1], matrix[0][2], matrix[0][3], matrix[0][4])
+
 print(matrix[1][4], matrix[2][4], matrix[3][4], matrix[4][4])
 print(matrix[4][3], matrix[4][2], matrix[4][1], matrix[4][0])
+
 print(matrix[3][0], matrix[2][0], matrix[1][0])
 print(matrix[1][1], matrix[1][2], matrix[1][3])
+
 print(matrix[2][3], matrix[3][3])
 print(matrix[3][2], matrix[3][1])
 print(matrix[2][1], matrix[2][2])
